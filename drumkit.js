40836338307cb3028aa162555c7952a81d0ff01a
Joshua Devon <joshua.devon@gmail.com>
2024-07-14 10:39:10 +1000

    for James

---
+++ drumkit.js
@@ -0,0 +1,17 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop function if no audio found.
+    audio.currentTime = 0; //allows to replay audio before finished.
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
